e49520afc5e7e4c32dfd04bc6b05e71fd8f12e72
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2023-01-19 19:36:08 +0400

    first commit

---
+++ src/test/ru.netology/CallBackTest.java
@@ -0,0 +1,22 @@
+public class CallBackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("Webdriver.chrome.driver". "C:\\Users\\Vadim\\IdeaProjects\\Selenium\\driver\\win")
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+    @Test
+    void shouldTestSomething() {
+        throw new UnsupportedOperationException();
+    }
+}
