896cd86430f249abb3dd1fa3ded53681c6377b98
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2023-01-28 18:30:55 +0400

    fixed2

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/0505hftcbwu3uqov/branch/main?svg=true)](https://ci.appveyor.com/project/mustafeev/selenium/branch/main)
+[![Build status](https://ci.appveyor.com/api/projects/status/ci6t1eq56gvevisd/branch/main?svg=true)](https://ci.appveyor.com/project/mustafeev/selenium-kxyid/branch/main)
--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -38,7 +38,7 @@ public class OrderTest {
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys(
         "+79278568743");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.cssSelector("button.button"));
+        driver.findElement(By.cssSelector("button.button")).click();
         var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
     }
